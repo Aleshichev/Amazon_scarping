eb24ce5607e4d071a6263dec5711d052b1fac394
Igor <Aleshichev.git@email.ua>
2022-06-01 13:17:24 +0300

    Скрапинг и отправка смс скрытие паролей

--- main.py
+++ main.py
@@ -6,14 +6,15 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import os
 
-SMTP_ADDRESS = "outlook.office365.com"
-MY_EMAIL = "Aleshichevigor@outlook.com"
-PASSWORD = "45rhfy7853rt"
-
+MYSMTP_ADDRESS = os.environ['SMTP_ADDRESS']
+MYMY_EMAIL = os.environ.get('MY_EMAIL')
+MYPASSWORD = os.environ.get('PASSWORD')
+MYUSER_AGENT = os.environ.get('USER_AGENT')
 url = "https://www.amazon.com/dp/B0053WRWX8/ref=sbl_dpx_kitchen-electric-cookware_B07NSTN2R4_0?th=1"
 header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.62 Safari/537.36",
+        "User-Agent": 'USER_AGENT',
         "Accept-Language": "en-US,en;q=0.9,ru;q=0.8"
 }
 response = requests.get(url, headers=header)     #создаём запрос на сайт адрес + язык
@@ -31,11 +32,11 @@ print(title)
 if price_as_float < 200:     # формируем условие
         message = f"{title} is now {price}"        #  формируем смс
 
-        with smtplib.SMTP("outlook.office365.com") as connection:
+        with smtplib.SMTP(MYSMTP_ADDRESS) as connection:
                 connection.starttls()       # защита письма
-                result = connection.login(MY_EMAIL, PASSWORD)     # подключаемся к почте
+                result = connection.login(MYMY_EMAIL, MYPASSWORD)     # подключаемся к почте
                 connection.sendmail(
-                        from_addr=MY_EMAIL,
-                        to_addrs=MY_EMAIL,
+                        from_addr=MYMY_EMAIL,
+                        to_addrs=MYMY_EMAIL,
                         msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}"
                 )                                 # отправляем письмо
